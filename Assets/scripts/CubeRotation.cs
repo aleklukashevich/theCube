bb3c669f205d0915f8c268cec08e691016d75192
Aliaksandr Lukashevich <aliaksandr.lukashevich@grandparade.co.uk>
2020-02-26 12:59:08 +0100

    add project

---
+++ Assets/scripts/CubeRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void OnMouseDrag()
+    {
+        transform.eulerAngles = Vector3.Lerp(this.transform.eulerAngles, new Vector3(0, -90f, 0), 2 * Time.deltaTime);
+    }
+}
